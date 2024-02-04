55330be233cbe324b6914a220c8c6b5c963fdb8b
Saurabh Talele <saurabhatalele@gmail.com>
2024-02-04 21:51:24 +0530

    init2

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -82,10 +82,10 @@ const Home = () => {
         <div className="flex flex-col lg:flex-row justify-between gap-10 py-6">
           <motion.div
             whileHover={{ scale: 1.1 }}
-            className="flex flex-col justify-center items-center lg:w-56 py-3 rounded-lg shadow-lg  shadow-slate-300"
+            className="flex flex-col justify-center items-center lg:w-56 h-80 py-3 rounded-lg shadow-lg  shadow-slate-300"
           >
-            <Image src={"/webd.png"} width={200} height={200} alt="webD" />
-            <h1 className="p-8 text-sm lg:text-[1.4vw] font-medium text-center">
+            <Image src={"/webd.png"} width={200} height={150} alt="webD" />
+            <h1 className="p-8 lg:text-[1rem] font-medium text-center">
               Web Design and Development
             </h1>
             <button className="bg-primary-red text-white px-3 py-1 rounded-full text-sm font-light">
@@ -97,7 +97,7 @@ const Home = () => {
             className="flex flex-col justify-center items-center lg:w-56 py-3 rounded-lg shadow-lg  shadow-slate-300"
           >
             <Image src={"/dm.png"} width={130} height={122} alt="webD" />
-            <h1 className="p-8 lg:text-[1.4vw] font-medium text-center">
+            <h1 className="p-8 lg:text-[1rem] font-medium text-center">
               Digital Marketing services
             </h1>
             <button className="bg-primary-red text-white px-3 py-1 rounded-full text-sm font-light">
@@ -109,7 +109,7 @@ const Home = () => {
             className="flex flex-col justify-center items-center lg:w-56 py-3 rounded-lg shadow-lg  shadow-slate-300"
           >
             <Image src={"/ec.png"} width={140} height={200} alt="webD" />
-            <h1 className="p-8 lg:text-[1.4vw] font-medium text-center">
+            <h1 className="p-8 lg:text-[1rem] font-medium text-center">
               E-commerce Services
             </h1>
             <button className="bg-primary-red text-white px-3 py-1 rounded-full text-sm font-light">
@@ -121,7 +121,7 @@ const Home = () => {
             className="flex flex-col justify-center items-center lg:w-56 py-3 rounded-lg shadow-lg  shadow-slate-300"
           >
             <Image src={"/branding.png"} width={130} height={200} alt="webD" />
-            <h1 className="p-8 lg:text-[1.4vw] font-medium text-center">
+            <h1 className="p-8 lg:text-[1rem] font-medium text-center">
               Branding & creative services
             </h1>
             <button className="bg-primary-red text-white px-3 py-1 rounded-full text-sm font-light">
